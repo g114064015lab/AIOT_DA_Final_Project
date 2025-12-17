7846d2de463bee5242fff8915e6165f6d326c3e4
g114064015lab <g114064015@mail.nchu.edu.tw>
2025-12-17 21:55:44 +0800

    Change logo letter color in app.py

--- app.py
+++ app.py
@@ -459,7 +459,7 @@ def main() -> None:
             font-weight:900;
             font-size:22px;
             letter-spacing:-0.5px;
-            color: rgb(230, 179, 157);
+            color: rgb(128, 99, 88);
             font-family: 'Canela', 'Space Grotesk', 'Segoe UI', sans-serif;
             z-index:2;
         }
