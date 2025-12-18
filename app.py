152d29f39cdec7571db47a393a9c7bc45caea25f
g114064015lab <g114064015@mail.nchu.edu.tw>
2025-12-18 11:05:18 +0800

    Update app.py

--- app.py
+++ app.py
@@ -521,7 +521,7 @@ def main() -> None:
             letter-spacing:-0.5px;
             color: rgb(128, 99, 88);
             font-family: 'Kunstler Script', cursive;
-            margin-right: 6px;
+            margin-right: 15px;
             z-index:1;
         }
         .logo-letter-sidebar {
